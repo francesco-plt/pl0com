63818fe6c91b22117792d9e32154b12e66ad5f5f
Francesco Pallotta <fra.pall8o@gmail.com>
2022-04-08 10:00:04 +0000

    minor fixes

--- compile.sh
+++ compile.sh
@@ -1,16 +1,16 @@
-#!/bin/env sh
+#!/bin/sh
 
-if [[$# -ne 2]]
-then
+if [ $# -ne 1 ]; then
     echo 'usage: compile.sh <SOURCE_FILE>'
-    exit 0
+    exit 1
 fi
 
 export SRC=$1
 
 echo 'generating assembly...'
-python3 main.py $SRC
+python3 main.py $SRC -o out.s > /dev/null
 echo 'done. compiling and linking...'
 arm-linux-gnueabi-gcc out.s runtime.c -g -static -march=armv6 -o out
 echo 'done. running the binary...'
+echo 'what follows is output from the executable:\n'
 qemu-arm -cpu arm1136 out
